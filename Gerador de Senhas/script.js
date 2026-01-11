9b40c9f7090de65ca6b1ed066ced05014ba0177b
Scarlletti <alyyyxx00@gmail.com>
2026-01-11 14:03:54 -0300

    Atualização de projeto

---
+++ Gerador de Senhas/script.js
@@ -0,0 +1,11 @@
+let formulario = document.getElementById("formulario");
+let maiusculas = document.getElementById("maiusculas");
+let minusculas = document.getElementById("minusculas");
+let simbolos = document.getElementById("simbolos");
+let numeros = document.getElementById("numeros");
+let erro = document.getElementById("erro");
+let senha = document.getElementById("senha");
+
+formulario.addEventListener("submit", function (e) {
+    e.preventDefault();
+});
